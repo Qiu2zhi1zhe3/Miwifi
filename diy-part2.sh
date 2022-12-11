9d7f9e1ea3f1e15d129f47f94e27089963b95fe4
qiu2zhi1zhe3 <qiu2zh1zhe3@gmail.com>
2022-12-11 15:02:17 +0700

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/Qiu2zhi1zhe3/g' package/base-files/files/bin/config_generate
+sed -i 's/UTC/<+07>-7/g' package/base-files/files/bin/config_generate
